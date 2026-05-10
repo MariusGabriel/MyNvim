cdf51eafa6f50e7f543184f2ef502d10a3df3871
MariusGabriel <mariuscmg92@yahoo.com>
2026-05-10 12:08:14 +0300

    todo update

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -16,6 +16,25 @@ return {
       todo_comments.jump_prev()
     end, { desc = "Previous todo comment" })
 
-    todo_comments.setup()
+    todo_comments.setup({
+      keywords = {
+        FIX  = { icon = " ", color = "error",   alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "hack" },
+        WARN = { icon = " ", color = "warning",  alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", color = "perf",     alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = " ", color = "hint",     alt = { "INFO" } },
+        TEST = { icon = "⏲ ", color = "test",   alt = { "TESTING", "PASSED", "FAILED" } },
+      },
+      colors = {
+        error   = { "#c86a5a" },
+        warning = { "#c8a84a" },
+        info    = { "#e8c46a" },
+        hint    = { "#7ec8a0" },
+        hack    = { "#c8956c" },
+        perf    = { "#7db5c8" },
+        test    = { "#9a7ab8" },
+      },
+    })
   end,
 }
